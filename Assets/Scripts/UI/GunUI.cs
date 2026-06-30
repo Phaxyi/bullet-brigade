8d90f1a7c547ec352813d64bdbaef56024f7a62d
Phaxyi <albinocory@gmail.com>
2026-06-30 17:04:20 +0800

    Fill

--- Assets/Scripts/UI/GunUI.cs
+++ Assets/Scripts/UI/GunUI.cs
@@ -1,10 +1,26 @@
+using System.Drawing;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace BulletBrigade {
 	public class GunUI : MonoBehaviour
 	{
-	
+		private Gun _gun;
+		private TMP_Text _text;
+
+		private void Awake()
+		{
+			_gun = GameObject.FindWithTag("Player").GetComponent<Entity>().GetComponent<Gun>();
+			_text = transform.Find("Text").GetComponent<TMP_Text>();
+		}
+
+		private void Update()
+		{
+			bool isReloading = (Time.time - _gun.LastReloadTime) < _gun.ReloadCooldown;
+			string currMagText = isReloading ? "0" : _gun.BulletsInMag.ToString();
+			string reloadText = isReloading ? "<size='34'>(reloading!)</size> " : "";
+
+			_text.text = $"{reloadText}{currMagText} / {_gun.MagazineSize}";
+		}
 	}
 }
