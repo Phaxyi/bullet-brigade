de2cad0ae227ac12ebf722549f738c612001bdc6
Phaxyi <albinocory@gmail.com>
2026-07-01 13:49:46 +0800

    draw line for wallpath

--- Assets/Scripts/Obstacles/WallPath.cs
+++ Assets/Scripts/Obstacles/WallPath.cs
@@ -7,21 +7,22 @@
 */
 
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BulletBrigade {
 	public class WallPath : MonoBehaviour
 	{
-		[SerializeField] private float _pathDuration = 1; 	// time taken for total path (looping), or from A -> B (not looping)
+		[SerializeField] private float _pathDuration = 1; 	// time taken for total path to revist same point
 		[SerializeField] private bool _useSineEasing; 		// easing between each point
-		[SerializeField] private bool _loop; 				// loop for >=3 point or simply reverse?
+		[SerializeField] private bool _loop; 				// loop for >2 points or simply reverse?
 
 		private float _startTime; 
 		private readonly List<Vector2> _points = new();
 		private readonly List<float> _intervals = new();
 		private readonly Dictionary<Transform, float> _wallPhaseMap = new();
 
-		private void Start()
+		private void Awake()
 		{
 			_startTime = Time.time;
 
@@ -33,6 +34,8 @@ namespace BulletBrigade {
 
 			// get points
 			Transform posHolder = transform.Find("Positions");
+			_loop = posHolder.childCount < 3 ? false : _loop;
+
 			if (_loop)
 			{
 				foreach (Transform posObj in posHolder)
@@ -72,6 +75,23 @@ namespace BulletBrigade {
 			{
 				_intervals.Add(interval / totalDist);
 			}
+
+			// render lines between points
+			LineRenderer line = GetComponent<LineRenderer>();
+			List<Vector3> posArray = default;
+
+			if (_loop)
+			{
+				posArray = listV2toV3(_points.ToList());
+			}
+			else
+			{
+				HashSet<Vector2> deduplicated = new(_points);
+				posArray = listV2toV3(deduplicated.ToList());
+			}
+
+			line.positionCount = posArray.Count;
+			line.SetPositions(posArray.ToArray());
 		}
 
 		private void Update()
@@ -82,11 +102,9 @@ namespace BulletBrigade {
 			
 			for (int i = _intervals.Count - 1; i >= 0; i--)
 			{
-				if (interval > _intervals[i])
-				{
-					p1Index = i;
-					break;
-				}
+				if (interval < _intervals[i]) continue;
+				p1Index = i;
+				break;
 			}
 
 			Vector2 point1 = _points[p1Index];
@@ -94,19 +112,32 @@ namespace BulletBrigade {
 			float t1 = _intervals[p1Index];
 			float t2 = _intervals[p1Index + 1];
 			float t = (interval - t1) / (t2 - t1);
-
-			// https://easings.net/#easeInOutSine
-			if (_useSineEasing)
-				t = -(Mathf.Cos(Mathf.PI * t) - 1) / 2;
+			
+			if (_useSineEasing) EaseIOSine(ref t);
 
 			// move wall(s)
 			foreach (var pair in _wallPhaseMap)
 			{
 				float phase = pair.Value;
-				Vector2 lerpPos = Vector2.Lerp(point1, point2, (t + phase) % 1);
+				Vector2 lerpPos = Vector2.Lerp(point1, point2, (t + phase) % 1); // TODO: set phase above !!!!!!!!!!!!!!
 
 				pair.Key.position = lerpPos;
 			}
 		}
+
+		private List<Vector3> listV2toV3(List<Vector2> original)
+		{
+			List<Vector3> convert = new(original.Capacity);
+			foreach (Vector2 v2 in original)
+			{
+				convert.Add(v2);
+			}
+
+			return convert;
+		}
+
+		// https://easings.net/#easeInOutSine
+		private float EaseIOSine(ref float t)
+			=> -(Mathf.Cos(Mathf.PI * t) - 1) / 2;
 	}
 }
